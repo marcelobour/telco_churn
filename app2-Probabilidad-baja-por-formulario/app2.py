86833a3a89b3096024c627203c3a6837cdbd2f75
marcelobour <marcelo.bour.ds@gmail.com>
2022-11-26 01:35:36 -0300

    Ajusta alineación y márgenes

--- app2-Probabilidad-baja-por-formulario/app2.py
+++ app2-Probabilidad-baja-por-formulario/app2.py
@@ -12,13 +12,13 @@ url_data='https://raw.githubusercontent.com/marcelobour/telco_churn/main/Telco_c
 #Ruta del modelo entrenado
 url_model='https://raw.githubusercontent.com/marcelobour/app2/main/aucpr-precision-8var.txt'
 
-#Generamos dataframe de ciudades y coordenadas
-coor_cols = ['City', 'Longitude', 'Latitude']
-telco = pd.read_csv(url_data, sep=';', usecols=coor_cols)
-to_num_cols = ['Longitude', 'Latitude']
-telco[to_num_cols] = telco[to_num_cols].apply(lambda x: x.str.replace(',', '.')).apply(lambda x: x.str.replace(' ', '0')).apply(lambda x: pd.to_numeric(x))
-cities_coor = telco.groupby(['City']).mean()
-cities = np.sort(telco['City'].unique())
+#Quitamos margen de arriba excesivo por defecto
+hide_streamlit_style = """
+<style>
+    #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 2rem;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 #Colocamos título
 st.title("Probabilidad de baja")
@@ -32,6 +32,14 @@ title_alignment="""
 </style>
 """
 st.markdown(title_alignment, unsafe_allow_html=True)
+
+#Generamos dataframe de ciudades y coordenadas
+coor_cols = ['City', 'Longitude', 'Latitude']
+telco = pd.read_csv(url_data, sep=';', usecols=coor_cols)
+to_num_cols = ['Longitude', 'Latitude']
+telco[to_num_cols] = telco[to_num_cols].apply(lambda x: x.str.replace(',', '.')).apply(lambda x: x.str.replace(' ', '0')).apply(lambda x: pd.to_numeric(x))
+cities_coor = telco.groupby(['City']).mean()
+cities = np.sort(telco['City'].unique())
 
 #Creamos los elementos para cargar el formulario de 8 variables en la barra lateral
 with st.sidebar:
@@ -144,3 +152,13 @@ else:
 #Mostramos instrucción
 if instruc is not None:
   st.subheader(instruc, anchor='instruccion')
+
+  #Centramos la instrucción
+  inst_alin="""
+  <style>
+  #instruccion {
+    text-align: center
+  }
+  </style>
+  """
+  st.markdown(inst_alin, unsafe_allow_html=True)
